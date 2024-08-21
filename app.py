afef83a38c099508a0e42edd871072bd56ef8536
earnify123 <earnify123@gmail.com>
2024-08-21 14:15:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class User(db.Model):
 # Home route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # Profile route
 @app.route('/profile')
